490c04398fc4c7a37b1d47a0ba95284d39090826
rlboyle <rlboyle77@gmail.com>
2024-07-21 21:46:44 -0700

    implemented AspectVGrid

---
+++ A3/SET/SET/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  SET
+//
+//  Created by Ryan Boyle on 7/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("SET!")
+                .font(.largeTitle)
+                .bold()
+            ScrollView {
+                
+            }
+            Button("Deal Three More Cards") {
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ A3/SET/SET/SETApp.swift
@@ -0,0 +1,17 @@
+//
+//  SETApp.swift
+//  SET
+//
+//  Created by Ryan Boyle on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SETApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
